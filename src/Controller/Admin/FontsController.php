6392380e3f7fd306d6d870459114c28985941973
roadiz-ci <ambroise+ci@roadiz.io>
2026-05-26 15:44:15 +0000

    chore: bumped

--- src/Controller/Admin/FontsController.php
+++ src/Controller/Admin/FontsController.php
@@ -4,16 +4,13 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\FontBundle\Controller\Admin;
 
-use Doctrine\Persistence\ManagerRegistry;
+use JMS\Serializer\SerializerInterface;
 use League\Flysystem\FilesystemException;
 use League\Flysystem\FilesystemOperator;
 use RZ\Roadiz\Core\AbstractEntities\PersistableInterface;
-use RZ\Roadiz\CoreBundle\ListManager\EntityListManagerFactoryInterface;
-use RZ\Roadiz\CoreBundle\Security\LogTrail;
 use RZ\Roadiz\FontBundle\Entity\Font;
 use RZ\Roadiz\FontBundle\Event\Font\PreUpdatedFontEvent;
 use RZ\Roadiz\FontBundle\Form\FontType;
-use RZ\Roadiz\RozierBundle\Controller\AbstractAdminController;
 use RZ\Roadiz\Utils\StringHandler;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,84 +18,68 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Contracts\EventDispatcher\Event;
-use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
+use Themes\Rozier\Controllers\AbstractAdminController;
 
 class FontsController extends AbstractAdminController
 {
     public function __construct(
         private readonly FilesystemOperator $fontStorage,
+        SerializerInterface $serializer,
         UrlGeneratorInterface $urlGenerator,
-        EntityListManagerFactoryInterface $entityListManagerFactory,
-        ManagerRegistry $managerRegistry,
-        TranslatorInterface $translator,
-        LogTrail $logTrail,
-        EventDispatcherInterface $eventDispatcher,
     ) {
-        parent::__construct($urlGenerator, $entityListManagerFactory, $managerRegistry, $translator, $logTrail, $eventDispatcher);
+        parent::__construct($serializer, $urlGenerator);
     }
 
-    #[\Override]
     protected function supports(PersistableInterface $item): bool
     {
         return $item instanceof Font;
     }
 
-    #[\Override]
     protected function getNamespace(): string
     {
         return 'font';
     }
 
-    #[\Override]
     protected function createEmptyItem(Request $request): PersistableInterface
     {
         return new Font();
     }
 
-    #[\Override]
     protected function getTemplateFolder(): string
     {
         return '@RoadizFont/admin';
     }
 
-    #[\Override]
     protected function getRequiredRole(): string
     {
         return 'ROLE_ACCESS_FONTS';
     }
 
-    #[\Override]
     protected function getEntityClass(): string
     {
         return Font::class;
     }
 
-    #[\Override]
     protected function getFormType(): string
     {
         return FontType::class;
     }
 
-    #[\Override]
     protected function getDefaultOrder(Request $request): array
     {
         return ['name' => 'ASC'];
     }
 
-    #[\Override]
     protected function getDefaultRouteName(): string
     {
         return 'fontsHomePage';
     }
 
-    #[\Override]
     protected function getEditRouteName(): string
     {
         return 'fontsEditPage';
     }
 
-    #[\Override]
     protected function createUpdateEvent(PersistableInterface $item): ?Event
     {
         if ($item instanceof Font) {
@@ -108,7 +89,6 @@ class FontsController extends AbstractAdminController
         return null;
     }
 
-    #[\Override]
     protected function getEntityName(PersistableInterface $item): string
     {
         if ($item instanceof Font) {
